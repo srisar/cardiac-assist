e0f42a121ac22d69208873c476555e1fb76cac57
M Srisaravana <saravanamuthaly@gmail.com>
2020-06-07 20:13:18 +0530

    added base view template file

---
+++ views/__base_template.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * View:
+ * Controller:
+ */
+
+use App\Core\App;
+
+?>
+<?php include_once BASE_PATH . '/views/_header.inc.php'; ?>
+
+<div class="container">
+
+    <div class="row justify-content-center">
+
+        <div class="col">
+
+
+
+        </div><!--col-->
+
+    </div><!--row-->
+
+</div><!--container-->
+
+<?php include_once BASE_PATH . '/views/_footer.inc.php'; ?>
+
