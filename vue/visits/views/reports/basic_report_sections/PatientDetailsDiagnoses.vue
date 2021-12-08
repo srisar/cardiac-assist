88bedfc9f8ed271569158a9b0e97d1227cfc51f1
Srisaravana Manicaraja <saravanamuthaly@gmail.com>
2021-12-08 11:12:02 +0530

    prod: 2.6.0 updated prescriptions section

--- vue/visits/views/reports/basic_report_sections/PatientDetailsDiagnoses.vue
+++ vue/visits/views/reports/basic_report_sections/PatientDetailsDiagnoses.vue
@@ -1,302 +1,316 @@
 <template>
-
-  <div>
-
-    <div v-if="loaded">
-      <!-- section: basic -->
-      <PatientBasicDetails/>
-
-      <!-- section: lipids details -->
-      <div id="section-lipids" v-if="! isLipidsEmpty">
-        <table class="table table-sm table-bordered">
-          <tbody>
-          <tr>
-            <td style="width: 20%">
-              <div><span class="font-weight-bold">TC:</span> {{ lipids.tc }}</div>
-            </td>
-            <td style="width: 20%">
-              <div><span class="font-weight-bold">LDL:</span> {{ lipids.ldl }}</div>
-            </td>
-            <td style="width: 20%">
-              <div><span class="font-weight-bold">HDL:</span> {{ lipids.hdl }}</div>
-            </td>
-            <td style="width: 20%">
-              <div><span class="font-weight-bold">TG:</span> {{ lipids.tg }}</div>
-            </td>
-            <td style="width: 20%">
-              <div><span class="font-weight-bold">NHC:</span> {{ lipids.nhc }}</div>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-
-
-      <!-- section: ecg -->
-      <div id="section-ecg" v-if="! isVisitECGEmpty">
-        <div class="lead text-uppercase font-weight-bold">ECG Details</div>
-        <table class="table table-sm table-borderless mb-1">
-          <tbody>
-          <tr>
-            <td>
-              <div class="prescription-details" v-html="visitECG.description" contenteditable="true"></div>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-        <hr>
-      </div>
-
-
-      <!-- section: clinical details -->
-      <div id="section-clinical-details" class="section" v-if="visitSymptoms.length > 0">
-        <div class="lead font-weight-bold text-uppercase">Clinical details</div>
-        <table class="table table-sm table-borderless mb-1">
-          <tbody>
-          <tr v-for="item in visitSymptoms">
-            <td contenteditable="true">{{ item.symptom.symptom_name }}</td>
-            <td contenteditable="true" class="text-right">{{ item.duration }}</td>
-          </tr>
-          </tbody>
-        </table>
-        <hr>
-      </div>
-
-      <!-- section: differential diagnoses -->
-      <div class="section-differential-diagnoses section" v-if="diffDiagnoses.length > 0">
-        <div class="lead font-weight-bold text-uppercase">Differential diagnoses</div>
-        <table class="table table-sm table-borderless mb-1">
-          <tbody>
-          <tr>
-            <td contenteditable="true">
+	
+	<div>
+		
+		<div v-if="loaded">
+			<!-- section: basic -->
+			<PatientBasicDetails/>
+			
+			<!-- section: lipids details -->
+			<div id="section-lipids" v-if="! isLipidsEmpty">
+				<table class="table table-sm table-bordered">
+					<tbody>
+					<tr>
+						<td style="width: 20%">
+							<div><span class="font-weight-bold">TC:</span> {{ lipids.tc }}</div>
+						</td>
+						<td style="width: 20%">
+							<div><span class="font-weight-bold">LDL:</span> {{ lipids.ldl }}</div>
+						</td>
+						<td style="width: 20%">
+							<div><span class="font-weight-bold">HDL:</span> {{ lipids.hdl }}</div>
+						</td>
+						<td style="width: 20%">
+							<div><span class="font-weight-bold">TG:</span> {{ lipids.tg }}</div>
+						</td>
+						<td style="width: 20%">
+							<div><span class="font-weight-bold">NHC:</span> {{ lipids.nhc }}</div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+			
+			
+			<!-- section: ecg -->
+			<div id="section-ecg" v-if="! isVisitECGEmpty">
+				<div class="lead text-uppercase font-weight-bold">ECG Details</div>
+				<table class="table table-sm table-borderless mb-1">
+					<tbody>
+					<tr>
+						<td>
+							<div class="prescription-details" v-html="visitECG.description" contenteditable="true"></div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+				<hr>
+			</div>
+			
+			
+			<!-- section: clinical details -->
+			<div id="section-clinical-details" class="section" v-if="visitSymptoms.length > 0">
+				<div class="lead font-weight-bold text-uppercase">Clinical details</div>
+				<table class="table table-sm table-borderless mb-1">
+					<tbody>
+					<tr v-for="item in visitSymptoms">
+						<td contenteditable="true">{{ item.symptom.symptom_name }}</td>
+						<td contenteditable="true" class="text-right">{{ item.duration }}</td>
+					</tr>
+					</tbody>
+				</table>
+				<hr>
+			</div>
+			
+			<!-- section: differential diagnoses -->
+			<div class="section-differential-diagnoses section" v-if="diffDiagnoses.length > 0">
+				<div class="lead font-weight-bold text-uppercase">Differential diagnoses</div>
+				<table class="table table-sm table-borderless mb-1">
+					<tbody>
+					<tr>
+						<td contenteditable="true">
               <span class="mr-2" v-for="(item, index) in diffDiagnoses">{{ item.disease.disease }}
                 <span v-if="item.remarks">&mdash; {{ item.remarks }}</span>
                 <span v-if="index !== diffDiagnoses.length - 1">&diams;</span>
               </span>
-            </td>
-          </tr>
-          </tbody>
-          <hr>
-        </table>
-      </div>
-      <!-- section: differential diagnoses -->
-
-
-      <!-- section: investigations -->
-      <div id="section-investigations" class="section" v-if="investigations.length > 0">
-        <div class="lead font-weight-bold text-uppercase">Investigations</div>
-        <table class="table table-sm table-borderless mb-1">
-          <tbody>
-          <tr v-for="item in investigations">
-            <td contenteditable="true">
-              <div><span class="font-weight-bold">{{ item.investigation.investigation_name }}</span> - {{ item.remarks }}</div>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-        <hr>
-      </div>
-
-      <!-- section: visit problems -->
-      <div id="section-problems" class="section" v-if="diagnoses.length > 0 || problems.length > 0">
-        <div class="lead font-weight-bold text-uppercase">Problems & Diagnoses</div>
-        <table class="table table-sm table-borderless mb-1">
-          <tbody>
-          <tr v-for="item in diagnoses">
-            <td>
-              <div contenteditable="true">
-                {{ item.disease.disease }}
-                <span v-if="item.remarks">&mdash; {{ item.remarks }}</span>
-              </div>
-            </td>
-          </tr>
-          <tr v-for="item in problems">
-            <td>
-              <div contenteditable="true">
-                {{ item.problem.problem }}
-                <span v-if="item.remarks">&mdash; {{ item.remarks }}</span>
-              </div>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-        <hr>
-      </div>
-      <!-- section: visit problems -->
-
-      <div class="section" id="section-further-investigations" v-if="furtherInvestigationsList.length > 0">
-        <div class="lead font-weight-bold text-uppercase">Further Investigations</div>
-        <table class="table table-sm table-borderless mb-1">
-          <tbody>
-          <tr v-for="item in furtherInvestigationsList">
-            <td contenteditable="true">
-              <div><span class="font-weight-bold">{{ item.investigation.investigation_name }}</span> - {{ item.remarks }}</div>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-
-
-    </div>
-
-    <div v-else>
-      <TheLoading/>
-    </div>
-
-
-  </div><!-- template -->
+						</td>
+					</tr>
+					</tbody>
+					<hr>
+				</table>
+			</div>
+			<!-- section: differential diagnoses -->
+			
+			
+			<!-- section: investigations -->
+			<div id="section-investigations" class="section" v-if="investigations.length > 0">
+				<div class="lead font-weight-bold text-uppercase">Investigations</div>
+				<table class="table table-sm table-borderless mb-1">
+					<tbody>
+					<tr v-for="item in investigations">
+						<td contenteditable="true">
+							<div><span class="font-weight-bold">{{ item.investigation.investigation_name }}</span> &mdash;
+								<span :class="renderAbnormalInvestigation(item.remarks)">{{ item.remarks }}</span></div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+				<hr>
+			</div>
+			
+			<!-- section: visit problems -->
+			<div id="section-problems" class="section" v-if="diagnoses.length > 0 || problems.length > 0">
+				<div class="lead font-weight-bold text-uppercase">Problems & Diagnoses</div>
+				<table class="table table-sm table-borderless mb-1">
+					<tbody>
+					<tr v-for="item in diagnoses">
+						<td>
+							<div contenteditable="true">
+								{{ item.disease.disease }}
+								<span v-if="item.remarks">&mdash; {{ item.remarks }}</span>
+							</div>
+						</td>
+					</tr>
+					<tr v-for="item in problems">
+						<td>
+							<div contenteditable="true">
+								{{ item.problem.problem }}
+								<span v-if="item.remarks">&mdash; {{ item.remarks }}</span>
+							</div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+				<hr>
+			</div>
+			<!-- section: visit problems -->
+			
+			<div class="section" id="section-further-investigations" v-if="furtherInvestigationsList.length > 0">
+				<div class="lead font-weight-bold text-uppercase">Further Investigations</div>
+				<table class="table table-sm table-borderless mb-1">
+					<tbody>
+					<tr v-for="item in furtherInvestigationsList">
+						<td contenteditable="true">
+							<div><span class="font-weight-bold">{{ item.investigation.investigation_name }}</span> - {{ item.remarks }}</div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		
+		
+		</div>
+		
+		<div v-else>
+			<TheLoading/>
+		</div>
+	
+	
+	</div><!-- template -->
 
 
 </template>
 
 <script>
 import {errorMessageBox} from '@/_common/bootbox_dialogs.js';
+import voca from 'voca';
 import TheLoading from '../../../../_common/components/TheLoading';
 import PatientBasicDetails from './PatientBasicDetails';
 
 export default {
-  name: 'PatientDetailsDiagnoses',
-  components: { PatientBasicDetails, TheLoading },
-  data() {
-    return {
-      loaded: false,
-    };
-  },
-
-  computed: {
-
-    visit() {
-      return this.$store.getters.getVisit;
-    },
-
-    patient() {
-      return this.$store.getters.getPatient;
-    },
-
-
-    patientAge() {
-      if ( !_.isEmpty( this.patient.dob ) ) {
-        const today = moment();
-        const diff = moment.duration( today.diff( moment( this.patient.dob ) ) );
-        return Math.round( diff.asYears() );
-      }
-      return '';
-    },
-
-    /* loaded modules */
-    visitSymptoms() {
-      return this.$store.getters.getVisitSymptomsList;
-    },
-
-    diffDiagnoses() {
-      return this.$store.getters.getDifferentialDiagnosisList;
-    },
-
-    investigations() {
-      return this.$store.getters.getVisitInvestigationsList;
-    },
-
-    diagnoses() {
-      return this.$store.getters.getVisitDiagnosisList;
-    },
-
-    problems() {
-      return this.$store.getters.getVisitProblemsList;
-    },
-
-    lipids() {
-      return this.$store.getters.getVisitLipids;
-    },
-
-    visitECG() {
-      return this.$store.getters.getVisitECG;
-    },
-
-    furtherInvestigationsList() {
-      return this.$store.getters.getFurtherInvestigationsList;
-    },
-
-    /* -------------------------------------------------------------------------------- */
-    /* validators */
-    /* -------------------------------------------------------------------------------- */
-
-    isLipidsEmpty() {
-      return this.lipids.hdl === 0 && this.lipids.ldl === 0 && this.lipids.nhc === 0 && this.lipids.tc === 0 && this.lipids.tg === 0;
-    },
-
-    isVisitECGEmpty() {
-      return this.visitECG.description === '';
-    },
-
-  },
-  /* -- computed -- */
-
-  filters: {
-
-    boolean( value ) {
-      if ( value ) return 'Yes';
-      return 'No';
-    },
-
-    filterSmoking( value ) {
-      switch ( value ) {
-        case 'NO':
-          return 'No';
-        case 'SMOKING':
-          return 'Smoking';
-        case 'JUST_QUIT':
-          return 'Just quit';
-        case 'EX_SMOKER':
-          return 'Ex smoker';
-      }
-    },
-
-    filterFamilyHistory( value ) {
-      switch ( value ) {
-        case 'Y':
-          return 'Yes';
-        case 'N' :
-          return 'No';
-      }
-    },
-
-  },
-  /* -- filters -- */
-
-
-  async mounted() {
-    try {
-
-      const visitId = this.$store.getters.getVisitId;
-
-      await this.$store.dispatch( 'visitSymptoms_fetchAll', visitId );
-      await this.$store.dispatch( 'diffDiagnoses_fetchAll', visitId );
-      await this.$store.dispatch( 'visitInvestigations_fetchAll', visitId );
-      await this.$store.dispatch( 'diagnoses_fetchAll', visitId );
-      await this.$store.dispatch( 'visitProblems_fetchAll', visitId );
-      await this.$store.dispatch( 'visitLipids_fetchAll', visitId );
-      await this.$store.dispatch( 'visitECG_fetch', visitId );
-
-      await this.$store.dispatch( 'furtherInvestigations_fetchAll', visitId );
-
-      this.loaded = true;
-
-    } catch ( e ) {
-      errorMessageBox( 'Failed to load required data' );
-    }
-
-  },
-  /* -- mounted -- */
-
-
+	name: 'PatientDetailsDiagnoses',
+	components: { PatientBasicDetails, TheLoading },
+	data() {
+		return {
+			loaded: false,
+		};
+	},
+	
+	computed: {
+		
+		visit() {
+			return this.$store.getters.getVisit;
+		},
+		
+		patient() {
+			return this.$store.getters.getPatient;
+		},
+		
+		
+		patientAge() {
+			if ( !_.isEmpty( this.patient.dob ) ) {
+				const today = moment();
+				const diff = moment.duration( today.diff( moment( this.patient.dob ) ) );
+				return Math.round( diff.asYears() );
+			}
+			return '';
+		},
+		
+		/* loaded modules */
+		visitSymptoms() {
+			return this.$store.getters.getVisitSymptomsList;
+		},
+		
+		diffDiagnoses() {
+			return this.$store.getters.getDifferentialDiagnosisList;
+		},
+		
+		investigations() {
+			return this.$store.getters.getVisitInvestigationsList;
+		},
+		
+		diagnoses() {
+			return this.$store.getters.getVisitDiagnosisList;
+		},
+		
+		problems() {
+			return this.$store.getters.getVisitProblemsList;
+		},
+		
+		lipids() {
+			return this.$store.getters.getVisitLipids;
+		},
+		
+		visitECG() {
+			return this.$store.getters.getVisitECG;
+		},
+		
+		furtherInvestigationsList() {
+			return this.$store.getters.getFurtherInvestigationsList;
+		},
+		
+		/* -------------------------------------------------------------------------------- */
+		/* validators */
+		/* -------------------------------------------------------------------------------- */
+		
+		isLipidsEmpty() {
+			return this.lipids.hdl === 0 && this.lipids.ldl === 0 && this.lipids.nhc === 0 && this.lipids.tc === 0 && this.lipids.tg === 0;
+		},
+		
+		isVisitECGEmpty() {
+			return this.visitECG.description === '';
+		},
+		
+	},
+	/* -- computed -- */
+	
+	filters: {
+		
+		boolean( value ) {
+			if ( value ) return 'Yes';
+			return 'No';
+		},
+		
+		filterSmoking( value ) {
+			switch ( value ) {
+				case 'NO':
+					return 'No';
+				case 'SMOKING':
+					return 'Smoking';
+				case 'JUST_QUIT':
+					return 'Just quit';
+				case 'EX_SMOKER':
+					return 'Ex smoker';
+			}
+		},
+		
+		filterFamilyHistory( value ) {
+			switch ( value ) {
+				case 'Y':
+					return 'Yes';
+				case 'N' :
+					return 'No';
+			}
+		},
+		
+	},
+	/* -- filters -- */
+	
+	
+	async mounted() {
+		try {
+			
+			const visitId = this.$store.getters.getVisitId;
+			
+			await this.$store.dispatch( 'visitSymptoms_fetchAll', visitId );
+			await this.$store.dispatch( 'diffDiagnoses_fetchAll', visitId );
+			await this.$store.dispatch( 'visitInvestigations_fetchAll', visitId );
+			await this.$store.dispatch( 'diagnoses_fetchAll', visitId );
+			await this.$store.dispatch( 'visitProblems_fetchAll', visitId );
+			await this.$store.dispatch( 'visitLipids_fetchAll', visitId );
+			await this.$store.dispatch( 'visitECG_fetch', visitId );
+			
+			await this.$store.dispatch( 'furtherInvestigations_fetchAll', visitId );
+			
+			this.loaded = true;
+			
+		} catch ( e ) {
+			errorMessageBox( 'Failed to load required data' );
+		}
+		
+	},
+	/* -- mounted -- */
+	
+	methods: {
+		
+		renderAbnormalInvestigation( value ) {
+			if ( voca.first( value ) === '*' ) {
+				return 'border-bottom border-dark';
+			}
+			return '';
+		},
+		
+		
+	},
+	
+	
 };
 </script>
 
 <style>
 
 .prescription-details > > > p {
-  margin-bottom: 0 !important;
+	margin-bottom: 0 !important;
 }
 
 
--- vue/visits/views/reports/basic_report_sections/PrescriptionDetails.vue
+++ vue/visits/views/reports/basic_report_sections/PrescriptionDetails.vue
@@ -1,101 +1,103 @@
 <template>
-
-  <div>
-
-    <div v-if="prescription">
-
-      <!-- prescriptions -->
-      <div class="lead text-uppercase font-weight-bold">Prescriptions</div>
-
-      <table class="table table-sm table-borderless">
-        <tr>
-          <td class="font-weight-bold">
-            {{ prescription.date }}
-            <div class="" v-if="prescription.remarks">{{ prescription.remarks }}</div>
-          </td>
-
-        </tr>
-      </table>
-
-      <!-- prescription items -->
-      <table class="table table-sm">
-        <thead>
-        <tr>
-          <th>Drug</th>
-          <th>Dose</th>
-          <th>Frequency</th>
-          <th>Duration</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="item in prescription.prescription_items" :key="item.id">
-          <td>{{ item.drug.drug_name }}</td>
-          <td>{{ item.dose }}</td>
-          <td>{{ item.frequency }}</td>
-          <td>{{ item.duration }}</td>
-        </tr>
-        </tbody>
-      </table>
-
-    </div>
-
-    <div v-if="!loaded">
-      <TheLoading/>
-    </div>
-
-
-  </div>
+	
+	<div>
+		
+		<div v-if="prescription">
+			
+			<!-- prescriptions -->
+			<div class="lead text-uppercase font-weight-bold">Prescriptions</div>
+			
+			<table class="table table-sm table-borderless">
+				<tr>
+					<td class="font-weight-bold">
+						{{ prescription.date }}
+						<div class="" v-if="prescription.remarks">{{ prescription.remarks }}</div>
+					</td>
+				
+				</tr>
+			</table>
+			
+			<!-- prescription items -->
+			<table class="table table-bordered table-sm">
+				<thead>
+				<tr>
+					<th>Drug</th>
+					<th>Dose</th>
+					<th>Frequency</th>
+					<th>Duration</th>
+					<th>Remarks</th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr v-for="item in prescription.prescription_items" :key="item.id">
+					<td>{{ item.drug.drug_name }}</td>
+					<td>{{ item.dose }}</td>
+					<td>{{ item.frequency }}</td>
+					<td>{{ item.duration }}</td>
+					<td>{{ item.remarks }}</td>
+				</tr>
+				</tbody>
+			</table>
+		
+		</div>
+		
+		<div v-if="!loaded">
+			<TheLoading/>
+		</div>
+	
+	
+	</div>
 
 </template>
 
 <script>
-import {errorMessageBox} from "../../../../_common/bootbox_dialogs";
-import TheLoading from "../../../../_common/components/TheLoading";
+import {errorMessageBox} from '@/_common/bootbox_dialogs.js';
+import TheLoading from '../../../../_common/components/TheLoading';
 
 export default {
-  name: "PrescriptionDetails",
-  components: { TheLoading },
-  data() {
-    return {
-      loaded: false,
-    }
-  },
-
-  computed: {
-    visit() {
-      return this.$store.getters.getVisit;
-    },
-
-    patient() {
-      return this.$store.getters.getPatient;
-    },
-
-    prescription() {
-      return this.$store.getters.getVisitPrescriptions[ 0 ];
-    }
-
-  },
-
-  async mounted() {
-
-    try {
-
-      const visitId = this.$store.getters.getVisitId;
-      await this.$store.dispatch( "prescriptions_fetchAll", visitId );
-
-      this.loaded = true;
-
-    } catch ( e ) {
-      errorMessageBox( "Failed to fetch prescription details" );
-    }
-
-  },
-
-  methods: {
-    //
-  },
-
-}
+	name: 'PrescriptionDetails',
+	components: { TheLoading },
+	data() {
+		return {
+			loaded: false,
+		};
+	},
+	
+	computed: {
+		visit() {
+			return this.$store.getters.getVisit;
+		},
+		
+		patient() {
+			return this.$store.getters.getPatient;
+		},
+		
+		prescription() {
+			return this.$store.getters.getVisitPrescriptions[ 0 ];
+		},
+		
+	},
+	
+	async mounted() {
+		
+		try {
+			
+			const visitId = this.$store.getters.getVisitId;
+			await this.$store.dispatch( 'prescriptions_fetchAll', visitId );
+			
+			this.loaded = true;
+			
+		} catch ( e ) {
+			errorMessageBox( 'Failed to fetch prescription details' );
+		}
+		
+	},
+	
+	methods: {
+		//
+	},
+	
+};
 </script>
 
 <style scoped lang="scss">
