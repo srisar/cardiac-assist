16defdd8db951e5ed1b76f9572a3ecfbefdebda3
Srisaravana Manicaraja <saravanamuthaly@gmail.com>
2021-12-09 19:51:05 +0530

    prod: 2.6.7 bugfix visits

--- vue/homepage/components/AppointmentsTable.vue
+++ vue/homepage/components/AppointmentsTable.vue
@@ -1,231 +1,258 @@
 <template>
-
-  <div class="">
-
-    <table class="table table-sm table-bordered">
-      <thead>
-      <tr :class="tableClass">
-        <th style="width: 220px">Patient</th>
-        <th class="text-center" style="width: 30px">Gender</th>
-        <th class="text-center" style="width: 135px">DoB/Age</th>
-        <th>Remarks</th>
-        <th class="text-center" style="width: 110px">Date</th>
-        <th class="text-center" style="width: 10px">Status</th>
-        <th style="width: 10px"></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="item in appointments">
-        <td>
-          <a :href="renderPatientUrl(item.patient.id)">
-            {{ item.patient.first_name }} {{ item.patient.last_name }}
-          </a>
-        </td>
-        <td class="text-center">{{ item.patient.gender | gender }}</td>
-        <td class="text-center">{{ item.patient.dob }} ({{ calculateAge( item.patient.dob ) }})</td>
-        <td>{{ item.remarks }}</td>
-        <td class="text-center">{{ item.date }}</td>
-        <td class="text-center" :class="renderStatusColor(item.status)" data-toggle="tooltip" data-placement="left" :title="item.status">
-          <img :src="getStatusIcon(item.status)" class="icon-16" alt="">
-        </td>
-        <td>
-          <button class="btn btn-tiny btn-outline-secondary" @click="onShowSelectedAppointment(item)">
-            <img src="/assets/images/actions/edit.svg" class="icon-16" alt="">
-          </button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
-
-    <ModalWindow :visible="modalAppointment.visible" @close="modalAppointment.visible = false">
-      <template v-slot:title>Edit Appointment</template>
-      <slot>
-
-        <div class="form-row justify-content-center">
-          <div class="col-4">
-            <div class="form-group">
-              <label>Date</label>
-              <DateField v-model="selectedAppointment.date"/>
-            </div>
-          </div>
-
-          <div class="col-4">
-            <div class="form-group">
-              <label>Status</label>
-              <input type="text" class="form-control" disabled :value="selectedAppointment.status">
-            </div>
-          </div>
-
-        </div>
-
-        <div class="form-row">
-          <div class="col">
-
-            <div class="form-group">
-              <label>Remarks</label>
-              <textarea rows="5" class="form-control" v-model.trim="selectedAppointment.remarks"></textarea>
-            </div>
-
-          </div>
-        </div>
-
-        <div class="text-center">
-          <button class="btn btn-success" @click="onUpdate()">
-            <img src="/assets/images/actions/save.svg" class="icon-24" alt=""> Update
-          </button>
-          <hr>
-
-          <div class="">
-            <button class="btn btn-sm btn-success" @click="onSetStatus(statuses.completed)">Set as completed</button>
-            <button class="btn btn-sm btn-info" @click="onSetStatus(statuses.cancelled)">Set as cancelled</button>
-            <button class="btn btn-sm btn-danger" @click="onSetStatus(statuses.missed)">Set as missed</button>
-            <button class="btn btn-sm btn-warning" @click="onSetStatus(statuses.pending)">Set as pending</button>
-          </div>
-
-        </div>
-
-
-      </slot>
-    </ModalWindow>
-
-  </div>
+	
+	<div class="">
+		
+		<table class="table table-sm table-bordered">
+			<thead>
+			<tr :class="tableClass">
+				<th style="width: 220px">Patient</th>
+				<th class="text-center" style="width: 30px">Gender</th>
+				<th class="text-center" style="width: 135px">DoB/Age</th>
+				<th>Remarks</th>
+				<th class="text-center" style="width: 110px">Date</th>
+				<th class="text-center" style="width: 10px">Status</th>
+				<th style="width: 10px"></th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr v-for="item in appointments">
+				<td>
+					<a :href="renderPatientUrl(item.patient.id)">
+						{{ item.patient.first_name }} {{ item.patient.last_name }}
+					</a>
+				</td>
+				<td class="text-center">{{ item.patient.gender | gender }}</td>
+				<td class="text-center">{{ item.patient.dob }} ({{ calculateAge( item.patient.dob ) }})</td>
+				<td>{{ item.remarks }}</td>
+				<td class="text-center">{{ item.date }}</td>
+				<td class="text-center" :class="renderStatusColor(item.status)" data-toggle="tooltip" data-placement="left" :title="item.status">
+					<img :src="getStatusIcon(item.status)" class="icon-16" alt="">
+				</td>
+				<td>
+					<div class="d-flex" style="gap: 5px">
+						
+						<button class="btn btn-tiny btn-success"
+										:class="{'d-none': item.status === statuses.completed}"
+										@click="onSetStatus(statuses.completed, item)"
+										title="Set as completed">
+							<img src="/assets/images/actions/done.svg" class="icon-16" alt="">
+						</button>
+						
+						<button class="btn btn-tiny btn-danger"
+										:class="{'d-none': item.status === statuses.missed}"
+										@click="onSetStatus(statuses.missed, item)"
+										title="Set as missed">
+							<img src="/assets/images/actions/warning.svg" class="icon-16" alt="">
+						</button>
+						
+						<button class="btn btn-tiny btn-info"
+										:class="{'d-none': item.status === statuses.cancelled}"
+										@click="onSetStatus(statuses.cancelled, item)"
+										title="Set as cancelled">
+							<img src="/assets/images/actions/question.svg" class="icon-16">
+						</button>
+						
+						<button class="btn btn-tiny btn-warning"
+										:class="{'d-none': item.status === statuses.pending}"
+										@click="onSetStatus(statuses.pending, item)"
+										title="Set as pending">
+							<img src="/assets/images/actions/in-progress.svg" class="icon-16">
+						</button>
+						
+						<button class="btn btn-tiny btn-outline-secondary"
+										@click="onShowSelectedAppointment(item)"
+										title="Edit appointment">
+							<img src="/assets/images/actions/edit.svg" class="icon-16" alt="">
+						</button>
+					</div>
+				</td>
+			</tr>
+			</tbody>
+		</table>
+		
+		<ModalWindow :visible="modalAppointment.visible" @close="modalAppointment.visible = false">
+			<template v-slot:title>Edit Appointment</template>
+			<slot>
+				
+				<div class="form-row justify-content-center">
+					<div class="col-4">
+						<div class="form-group">
+							<label>Date</label>
+							<DateField v-model="selectedAppointment.date"/>
+						</div>
+					</div>
+					
+					<div class="col-4">
+						<div class="form-group">
+							<label>Status</label>
+							<input type="text" class="form-control" disabled :value="selectedAppointment.status">
+						</div>
+					</div>
+				
+				</div>
+				
+				<div class="form-row">
+					<div class="col">
+						
+						<div class="form-group">
+							<label>Remarks</label>
+							<textarea rows="5" class="form-control" v-model.trim="selectedAppointment.remarks"></textarea>
+						</div>
+					
+					</div>
+				</div>
+				
+				<div class="text-center">
+					<button class="btn btn-success" @click="onUpdate()">
+						<img src="/assets/images/actions/save.svg" class="icon-24" alt=""> Update
+					</button>
+				</div>
+			
+			
+			</slot>
+		</ModalWindow>
+	
+	</div>
 
 
 </template>
 
 <script>
-import ModalWindow from "../../_common/components/ModalWindow";
-import DateField from "../../_common/components/DateField";
-import {errorMessageBox} from "../../_common/bootbox_dialogs";
+import {errorMessageBox} from '@/_common/bootbox_dialogs.js';
+import DateField from '../../_common/components/DateField';
+import ModalWindow from '../../_common/components/ModalWindow';
 
 export default {
-  name: "AppointmentsTable",
-  components: { DateField, ModalWindow },
-  props: ["appointments", "tableClass", "colored"],
-
-  data() {
-    return {
-      modalAppointment: {
-        visible: false,
-      },
-
-      selectedAppointment: {
-        id: null,
-        patient_id: null,
-        date: "",
-        remarks: "",
-        status: ""
-      },
-    }
-  },
-
-
-  computed: {
-    statuses() {
-      return this.$store.getters.getAppointmentStatuses;
-    }
-  },
-
-  filters: {
-    gender( value ) {
-      if ( value === "MALE" ) return "M";
-      if ( value === "FEMALE" ) return "F";
-      if ( value === "OTHER" ) return "O";
-      return value;
-    }
-  },
-
-  mounted() {
-    $( function () {
-      $( '[data-toggle="tooltip"]' ).tooltip();
-    } );
-
-  },
-
-  methods: {
-
-    calculateAge( dob ) {
-      const today = moment();
-      const diff = moment.duration( today.diff( moment( dob ) ) );
-      return Math.round( diff.asYears() );
-    },
-
-    renderPatientUrl( id ) {
-      return `${ getSiteURL() }/app/patients/edit.php?id=${ id }`;
-    },
-
-    renderStatusColor( status ) {
-      if ( this.colored ) {
-        if ( status === this.statuses.completed ) return "table-success";
-        if ( status === this.statuses.pending ) return "table-warning";
-        if ( status === this.statuses.cancelled ) return "table-danger";
-        if ( status === this.statuses.missed ) return "table-info";
-      }
-    },
-
-    /* ------------------------------------------- */
-
-    onShowSelectedAppointment( appointment ) {
-      this.selectedAppointment = appointment;
-      this.modalAppointment.visible = true;
-    },
-
-    /* ------------------------------------------- */
-
-    async onSetStatus( status ) {
-      try {
-        const params = {
-          id: this.selectedAppointment.id,
-          status: status
-        };
-
-        await this.$store.dispatch( "appointments_setStatus", params );
-
-        this.modalAppointment.visible = false;
-        this.$emit( "status-updated" );
-      } catch ( e ) {
-
-      }
-    },
-
-    getStatusIcon( status ) {
-      if ( status === this.statuses.completed ) {
-        return "/assets/images/actions/done.svg";
-      } else if ( status === this.statuses.pending ) {
-        return "/assets/images/actions/in-progress.svg";
-      } else if ( status === this.statuses.missed ) {
-        return "/assets/images/actions/warning.svg";
-      } else if ( status === this.statuses.cancelled ) {
-        return "/assets/images/actions/question.svg";
-      }
-    },
-
-
-    async onUpdate() {
-      try {
-
-        const params = {
-          id: this.selectedAppointment.id,
-          date: this.selectedAppointment.date,
-          remarks: this.selectedAppointment.remarks,
-          status: this.selectedAppointment.status,
-        };
-
-        await this.$store.dispatch( "appointments_update", params );
-        this.modalAppointment.visible = false;
-
-        this.$emit( "status-updated" );
-
-      } catch ( e ) {
-        errorMessageBox( "Failed to update appointment details" );
-      }
-    },
-
-  },
-
-}
+	name: 'AppointmentsTable',
+	components: { DateField, ModalWindow },
+	props: ['appointments', 'tableClass', 'colored'],
+	
+	data() {
+		return {
+			modalAppointment: {
+				visible: false,
+			},
+			
+			selectedAppointment: {
+				id: null,
+				patient_id: null,
+				date: '',
+				remarks: '',
+				status: '',
+			},
+		};
+	},
+	
+	
+	computed: {
+		statuses() {
+			return this.$store.getters.getAppointmentStatuses;
+		},
+	},
+	
+	filters: {
+		gender( value ) {
+			if ( value === 'MALE' ) return 'M';
+			if ( value === 'FEMALE' ) return 'F';
+			if ( value === 'OTHER' ) return 'O';
+			return value;
+		},
+	},
+	
+	mounted() {
+		
+		$( '[data-toggle="tooltip"]' ).tooltip( {
+			trigger: 'hover',
+		} );
+		
+		
+	},
+	
+	methods: {
+		
+		calculateAge( dob ) {
+			const today = moment();
+			const diff = moment.duration( today.diff( moment( dob ) ) );
+			return Math.round( diff.asYears() );
+		},
+		
+		renderPatientUrl( id ) {
+			return `${ getSiteURL() }/app/patients/edit.php?id=${ id }`;
+		},
+		
+		renderStatusColor( status ) {
+			if ( this.colored ) {
+				if ( status === this.statuses.completed ) return 'table-success';
+				if ( status === this.statuses.pending ) return 'table-warning';
+				if ( status === this.statuses.cancelled ) return 'table-danger';
+				if ( status === this.statuses.missed ) return 'table-info';
+			}
+		},
+		
+		/* ------------------------------------------- */
+		
+		onShowSelectedAppointment( appointment ) {
+			this.selectedAppointment = appointment;
+			this.modalAppointment.visible = true;
+		},
+		
+		/* ------------------------------------------- */
+		
+		async onSetStatus( status, item ) {
+			
+			try {
+				const params = {
+					id: item.id,
+					status: status,
+				};
+				
+				await this.$store.dispatch( 'appointments_setStatus', params );
+				
+				this.modalAppointment.visible = false;
+				this.$emit( 'status-updated' );
+			} catch ( e ) {
+			
+			}
+		},
+		
+		getStatusIcon( status ) {
+			if ( status === this.statuses.completed ) {
+				return '/assets/images/actions/done.svg';
+			} else if ( status === this.statuses.pending ) {
+				return '/assets/images/actions/in-progress.svg';
+			} else if ( status === this.statuses.missed ) {
+				return '/assets/images/actions/warning.svg';
+			} else if ( status === this.statuses.cancelled ) {
+				return '/assets/images/actions/question.svg';
+			}
+		},
+		
+		
+		async onUpdate() {
+			try {
+				
+				const params = {
+					id: this.selectedAppointment.id,
+					date: this.selectedAppointment.date,
+					remarks: this.selectedAppointment.remarks,
+					status: this.selectedAppointment.status,
+				};
+				
+				await this.$store.dispatch( 'appointments_update', params );
+				this.modalAppointment.visible = false;
+				
+				this.$emit( 'status-updated' );
+				
+			} catch ( e ) {
+				errorMessageBox( 'Failed to update appointment details' );
+			}
+		},
+		
+	},
+	
+};
 </script>
 
-<style scoped>
+<style>
 
 </style>
--- vue/homepage/views/AppointmentsView.vue
+++ vue/homepage/views/AppointmentsView.vue
@@ -1,207 +1,209 @@
 <template>
-
-  <div>
-    <div class="container">
-      <div class="row">
-        <div class="col">
-
-          <SingleDayAppointments/>
-
-          <div class="card shadow shadow-sm mb-3">
-            <div class="card-header">{{ customDateRangeLabel }}</div>
-            <div class="card-body">
-
-              <div class="mb-3 text-center">
-                <button class="btn btn-sm btn-primary" @click="modalCustomDateRange.visible=true">Choose date range</button>
-              </div>
-
-              <div v-if="appointmentsBetweenDates.length > 0">
-
-                <div class="mb-3 alert alert-dark text-center">
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="chk_all" name="chk_status" value="ALL" v-model="checkStatus">
-                    <label class="form-check-label" for="chk_all">
-                      All
-                    </label>
-                  </div>
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="chk_pending" name="chk_status" value="PENDING" v-model="checkStatus">
-                    <label class="form-check-label" for="chk_pending">
-                      Pending
-                    </label>
-                  </div>
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="chk_completed" name="chk_status" value="COMPLETED" v-model="checkStatus">
-                    <label class="form-check-label" for="chk_completed">
-                      Completed
-                    </label>
-                  </div>
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="chk_cancelled" name="chk_status" value="CANCELLED" v-model="checkStatus">
-                    <label class="form-check-label" for="chk_cancelled">
-                      Cancelled
-                    </label>
-                  </div>
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="chk_missed" name="chk_status" value="MISSED" v-model="checkStatus">
-                    <label class="form-check-label" for="chk_missed">
-                      Missed
-                    </label>
-                  </div>
-                </div><!-- filter radio buttons -->
-
-                <AppointmentsTable
-                    @status-updated="onBetweenDatesStatusUpdated()"
-                    :appointments="filteredAppointmentsBetweenDates"
-                    :colored="true"/>
-              </div><!-- section -->
-
-              <div v-else class="text-center">
-                No results.
-              </div>
-
-
-            </div>
-          </div>
-
-
-        </div><!-- col -->
-      </div><!-- row -->
-    </div><!-- container -->
-
-
-    <ModalWindow :visible="modalCustomDateRange.visible" @close="modalCustomDateRange.visible = false" size="small">
-      <template v-slot:title>Choose date range</template>
-      <div class="form-row justify-content-center">
-        <div class="col">
-          <div class="form-group text-center">
-            <DateRangeField v-model="dateRange" class="text-center"/>
-          </div>
-        </div>
-      </div>
-
-      <div class="text-center">
-        <button class="btn btn-primary" @click="onCustomDateRangeChoice()">
-          <img src="/assets/images/actions/done.svg" class="icon-24" alt=""> Choose
-        </button>
-      </div>
-
-    </ModalWindow>
-
-  </div><!-- template -->
+	
+	<div>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					
+					<!-- Today's appointments -->
+					<SingleDayAppointments/>
+					
+					
+					<div class="card shadow shadow-sm mb-3">
+						<div class="card-header">{{ customDateRangeLabel }}</div>
+						<div class="card-body">
+							
+							<div class="mb-3 text-center">
+								<button class="btn btn-sm btn-primary" @click="modalCustomDateRange.visible=true">Choose date range</button>
+							</div>
+							
+							<div v-if="appointmentsBetweenDates.length > 0">
+								
+								<div class="mb-3 alert alert-dark text-center">
+									
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="radio" id="chk_all" name="chk_status" value="ALL" v-model="checkStatus">
+										<label class="form-check-label" for="chk_all">
+											All
+										</label>
+									</div>
+									
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="radio" id="chk_pending" name="chk_status" value="PENDING" v-model="checkStatus">
+										<label class="form-check-label" for="chk_pending">
+											Pending
+										</label>
+									</div>
+									
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="radio" id="chk_completed" name="chk_status" value="COMPLETED" v-model="checkStatus">
+										<label class="form-check-label" for="chk_completed">
+											Completed
+										</label>
+									</div>
+									
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="radio" id="chk_cancelled" name="chk_status" value="CANCELLED" v-model="checkStatus">
+										<label class="form-check-label" for="chk_cancelled">
+											Cancelled
+										</label>
+									</div>
+									
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="radio" id="chk_missed" name="chk_status" value="MISSED" v-model="checkStatus">
+										<label class="form-check-label" for="chk_missed">
+											Missed
+										</label>
+									</div>
+								</div><!-- filter radio buttons -->
+								
+								<AppointmentsTable
+										@status-updated="onBetweenDatesStatusUpdated()"
+										:appointments="filteredAppointmentsBetweenDates"
+										:colored="true"/>
+							</div><!-- section -->
+							
+							<div v-else class="text-center">
+								No results.
+							</div>
+						
+						
+						</div>
+					</div>
+				
+				
+				</div><!-- col -->
+			</div><!-- row -->
+		</div><!-- container -->
+		
+		
+		<ModalWindow :visible="modalCustomDateRange.visible" @close="modalCustomDateRange.visible = false" size="small">
+			<template v-slot:title>Choose date range</template>
+			<div class="form-row justify-content-center">
+				<div class="col">
+					<div class="form-group text-center">
+						<DateRangeField v-model="dateRange" class="text-center"/>
+					</div>
+				</div>
+			</div>
+			
+			<div class="text-center">
+				<button class="btn btn-primary" @click="onCustomDateRangeChoice()">
+					<img src="/assets/images/actions/done.svg" class="icon-24" alt=""> Choose
+				</button>
+			</div>
+		
+		</ModalWindow>
+	
+	</div><!-- template -->
 
 </template>
 
 <script>
-import AppointmentsTable from "../components/AppointmentsTable";
-import DateRangeField from "../../_common/components/DateRangeField";
-import ModalWindow from "../../_common/components/ModalWindow";
-import {DateTime} from "luxon";
-import SingleDayAppointments from "./appointments/SingleDayAppointments";
+import {DateTime} from 'luxon';
+import DateRangeField from '../../_common/components/DateRangeField';
+import ModalWindow from '../../_common/components/ModalWindow';
+import AppointmentsTable from '../components/AppointmentsTable';
+import SingleDayAppointments from './appointments/SingleDayAppointments';
 
 export default {
-  name: "AppointmentsView",
-  components: { SingleDayAppointments, ModalWindow, DateRangeField, AppointmentsTable },
-  data() {
-    return {
-
-      today: DateTime.now().toFormat( "yyyy-MM-dd" ),
-
-      /* custom date range appointments */
-      modalCustomDateRange: {
-        visible: false,
-      },
-      dateRange: {
-        startDate: DateTime.now().minus( { days: 7 } ).toFormat( "yyyy-MM-dd" ),
-        endDate: DateTime.now().toFormat( "yyyy-MM-dd" )
-      },
-      checkStatus: "ALL",
-      appointmentsBetweenDates: [],
-      customDateRangeLabel: "Appointments between dates",
-
-
-    }
-  },
-
-
-  computed: {
-    statuses() {
-      return this.$store.getters.getAppointmentStatuses;
-    },
-
-    filteredAppointmentsBetweenDates() {
-
-      if ( this.checkStatus === "ALL" ) return this.appointmentsBetweenDates;
-
-      return _.filter( this.appointmentsBetweenDates, ( o ) => {
-        return o.status === this.checkStatus;
-      } );
-
-    }
-
-  },
-
-  filters: {},
-
-  async mounted() {
-
-    /* luxon test */
-    // console.log( DateTime.now().toFormat( "yyyy-MM-dd" ) );
-    // console.log( DateTime.now().plus( { days: 7 } ).toFormat( "yyyy-MM-dd" ) );
-    // console.log( DateTime.now().toISO() );
-
-    try {
-      await this.__fetchAppointments();
-    } catch ( e ) {
-    }
-
-  },
-
-  methods: {
-
-
-    async onBetweenDatesStatusUpdated() {
-      try {
-        const params = {
-          start_date: this.dateRange.startDate,
-          end_date: this.dateRange.endDate,
-        }
-
-        this.appointmentsBetweenDates = await this.$store.dispatch( "appointments_fetchBetweenDates", params );
-      } catch ( e ) {
-
-      }
-    },
-
-    async onCustomDateRangeChoice() {
-
-      try {
-
-        const params = {
-          start_date: this.dateRange.startDate,
-          end_date: this.dateRange.endDate,
-        }
-
-        this.appointmentsBetweenDates = await this.$store.dispatch( "appointments_fetchBetweenDates", params );
-
-        this.customDateRangeLabel = `Appointments between ${ params.start_date } and ${ params.end_date }`;
-
-        this.modalCustomDateRange.visible = false;
-      } catch ( e ) {
-
-      }
-
-    },
-
-
-  },
-
-}
+	name: 'AppointmentsView',
+	components: { SingleDayAppointments, ModalWindow, DateRangeField, AppointmentsTable },
+	data() {
+		return {
+			
+			today: DateTime.now().toFormat( 'yyyy-MM-dd' ),
+			
+			/* custom date range appointments */
+			modalCustomDateRange: {
+				visible: false,
+			},
+			dateRange: {
+				startDate: DateTime.now().minus( { days: 7 } ).toFormat( 'yyyy-MM-dd' ),
+				endDate: DateTime.now().toFormat( 'yyyy-MM-dd' ),
+			},
+			checkStatus: 'ALL',
+			appointmentsBetweenDates: [],
+			customDateRangeLabel: 'Appointments between dates',
+			
+			
+		};
+	},
+	
+	
+	computed: {
+		statuses() {
+			return this.$store.getters.getAppointmentStatuses;
+		},
+		
+		filteredAppointmentsBetweenDates() {
+			
+			if ( this.checkStatus === 'ALL' ) return this.appointmentsBetweenDates;
+			
+			return _.filter( this.appointmentsBetweenDates, ( o ) => {
+				return o.status === this.checkStatus;
+			} );
+			
+		},
+		
+	},
+	
+	filters: {},
+	
+	async mounted() {
+		
+		/* luxon test */
+		// console.log( DateTime.now().toFormat( "yyyy-MM-dd" ) );
+		// console.log( DateTime.now().plus( { days: 7 } ).toFormat( "yyyy-MM-dd" ) );
+		// console.log( DateTime.now().toISO() );
+		
+		try {
+			await this.__fetchAppointments();
+		} catch ( e ) {
+		}
+		
+	},
+	
+	methods: {
+		
+		
+		async onBetweenDatesStatusUpdated() {
+			try {
+				const params = {
+					start_date: this.dateRange.startDate,
+					end_date: this.dateRange.endDate,
+				};
+				
+				this.appointmentsBetweenDates = await this.$store.dispatch( 'appointments_fetchBetweenDates', params );
+			} catch ( e ) {
+			
+			}
+		},
+		
+		async onCustomDateRangeChoice() {
+			
+			try {
+				
+				const params = {
+					start_date: this.dateRange.startDate,
+					end_date: this.dateRange.endDate,
+				};
+				
+				this.appointmentsBetweenDates = await this.$store.dispatch( 'appointments_fetchBetweenDates', params );
+				
+				this.customDateRangeLabel = `Appointments between ${ params.start_date } and ${ params.end_date }`;
+				
+				this.modalCustomDateRange.visible = false;
+			} catch ( e ) {
+			
+			}
+			
+		},
+		
+		
+	},
+	
+};
 </script>
 
 <style scoped>
